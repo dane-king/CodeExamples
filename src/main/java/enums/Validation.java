37a9bc2935fda89d11ff284712727fd272e821de
kingd9 <kingd9@gmail.com>
2012-11-03 07:43:28 -0400

    Enum Template Pattern using validation

---
+++ src/main/java/enums/Validation.java
@@ -0,0 +1,23 @@
+package enums;
+
+import java.util.regex.Pattern;
+
+public enum Validation {
+	Email(){
+		@Override
+		public boolean validate(String value){
+			return EMAIL_PATTERN.matcher(value).matches();
+		}
+	},
+	Name(){
+		@Override
+		public boolean validate(String value){
+			return NAME_PATTERN.matcher(value).matches();
+		}
+	};
+	//not complete regex patterns for validation just an example
+	
+	private static final Pattern EMAIL_PATTERN = Pattern.compile("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
+	private static final Pattern NAME_PATTERN= Pattern.compile("^[A-Z][-'a-zA-Z]+$");
+	public abstract boolean validate(String value);
+}
