839790e8b195bae3349e92fbc1b4fa07d08379a2
kingd9 <kingd9@gmail.com>
2012-11-03 06:17:10 -0400

    Added Builder Pattern, created pattern directory for pattern examples

---
+++ src/test/java/patterns/creational/BuilderTest.java
@@ -0,0 +1,36 @@
+package patterns.creational;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+import org.drools.core.util.StringUtils;
+import org.junit.Test;
+
+public class BuilderTest {
+
+	@Test
+	public void testCreationOnlyRequired() {
+		Builder build=new Builder.InternalBuilder("required1", "required2").build();
+		assertEquals("required1",build.getRequired1());
+		assertEquals("required2",build.getRequired2());
+		
+	}
+	@Test
+	public void testCreationOnlyRequiredOneOptional() {
+		Builder build=new Builder.InternalBuilder("required1", "required2").option1("option1").build();
+		assertFalse(StringUtils.isEmpty(build.getRequired1()));
+		assertFalse(StringUtils.isEmpty(build.getRequired2()));
+		assertEquals("option1",build.getOptional1());
+		
+	}
+	@Test
+	public void testCreationOnlyRequiredThreeOptional() {
+		Builder build=new Builder.InternalBuilder("required1", "required2").option1("option1").option2("option2").option3("option3").build();
+		assertFalse(StringUtils.isEmpty(build.getRequired1()));
+		assertFalse(StringUtils.isEmpty(build.getRequired2()));
+		assertEquals("option1",build.getOptional1());
+		assertEquals("option2",build.getOptional2());
+		assertEquals("option3",build.getOptional3());
+		
+	}
+}
