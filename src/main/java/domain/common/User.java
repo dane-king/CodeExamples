bfa22c60a030baae719f70d15bcb92a7e5192179
kingd9 <kingd9@gmail.com>
2013-02-26 05:50:19 -0500

    Fixed Hashcode and Equals

--- src/main/java/domain/common/User.java
+++ src/main/java/domain/common/User.java
@@ -1,56 +1,55 @@
 package domain.common;
 
-public class User implements Comparable<User>{
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+public class User implements Comparable<User> {
 	@Override
 	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result
-				+ ((authorized == null) ? 0 : authorized.hashCode());
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		return result;
+		return new HashCodeBuilder().append(this.name).append(this.isAuthorized()).hashCode();
+
 	}
+
 	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
+	public boolean equals(final Object obj) {
+		if (obj instanceof User == false) {
+			return false;
+		}
+
+		if (this == obj) {
 			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
+		}
 		User other = (User) obj;
-		if (authorized == null) {
-			if (other.authorized != null)
-				return false;
-		} else if (!authorized.equals(other.authorized))
-			return false;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
-		return true;
+		return new EqualsBuilder().append(this.name, other.name).append(this.isAuthorized(), other.isAuthorized())
+				.isEquals();
 	}
-	String name;
-	Boolean authorized;
+
+	private String name;
+	private Boolean authorized;
+
 	public String getName() {
 		return name;
 	}
-	public void setName(String name) {
+
+	public void setName(final String name) {
 		this.name = name;
 	}
+
 	public Boolean isAuthorized() {
 		return authorized;
 	}
-	public void setAuthorized(Boolean authorized) {
+
+	public void setAuthorized(final Boolean authorized) {
 		this.authorized = authorized;
 	}
-	public User(String name, Boolean authorized) {
+
+	public User(final String name, final Boolean authorized) {
 		super();
 		this.name = name;
 		this.authorized = authorized;
 	}
-	public int compareTo(User user) {
+
+	public int compareTo(final User user) {
 		return this.getName().compareTo(user.getName());
 	}
 
