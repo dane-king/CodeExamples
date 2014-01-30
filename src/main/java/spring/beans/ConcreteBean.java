a053f5dbd5e8dcd11f86ed11106f276cef79fc0a
Dane King <kingd9@gmail.com>
2014-01-30 06:24:51 -0500

    adding spring examples, to show how to test with spring

---
+++ src/main/java/spring/beans/ConcreteBean.java
@@ -0,0 +1,15 @@
+package spring.beans;
+
+public class ConcreteBean implements IBean {
+
+	private String it;
+
+	public String getIt() {
+		return this.it;
+	}
+
+	public void setIt(final String it) {
+		this.it = it;
+	}
+
+}
