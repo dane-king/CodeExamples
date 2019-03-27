d653c79cfd9cc73213f3905df3f3a128466fc0c5
dane <kingd9@gmail.com>
2019-03-27 14:53:20 -0400

    more on concurrent test

--- src/test/java/threading/TestObjTest.java
+++ src/test/java/threading/TestObjTest.java
@@ -3,16 +3,17 @@ package threading;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import java.time.Instant;
-import java.time.LocalDate;
 import java.util.*;
 import java.util.concurrent.*;
+import java.util.function.Function;
+import java.util.function.IntFunction;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static junit.framework.TestCase.assertTrue;
-import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertEquals;
 
 @RunWith(MockitoJUnitRunner.class)
 public class TestObjTest {
@@ -23,7 +24,7 @@ public class TestObjTest {
     }
 
     @Test
-    public void shouldPass() throws InterruptedException {
+    public void testConcurrent1() throws InterruptedException {
         List<Callable> tasks= Arrays.asList(
                 () -> TestObj.parseDate("10/3/2011"),
                 ()->TestObj.parseDate("3/13/2004"),
@@ -39,6 +40,11 @@ public class TestObjTest {
         );
         assertConcurrent("Test me", tasks, 100);
 
+    }
+
+    @Test
+    public void testConcurrent2() throws Exception {
+        assertThreadsEqual(100,() -> TestObj.parseDate("12/1/2012"));
     }
 
     public static void assertConcurrent(final String message, final List<? extends Callable> runnables, final int maxTimeoutSeconds) throws InterruptedException {
@@ -73,5 +79,45 @@ public class TestObjTest {
 
         //assert no exceptions, if client is catching exceptions might just see stack trace
         assertTrue(message + "failed with exception(s)" + exceptions, exceptions.isEmpty());
+    }
+
+
+    private void assertThreadsEqual(final int threadCount, Callable<Date> task) throws Exception {
+        List<Callable<Date>> tasks = Collections.nCopies(threadCount, task);
+        ExecutorService executorService = Executors.newFixedThreadPool(threadCount);
+        List<Future<Date>> futures = executorService.invokeAll(tasks);
+        List<Date> resultList = futures
+                .stream()
+                .map(getFuture())
+                .collect(Collectors.toList());
+        // Validate the IDs
+        assertEquals(threadCount, futures.size());
+        IntFunction<Date> dateIntFunction = (num) -> {
+            try {
+                return task.call();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        };
+        List<Date> expectedList = IntStream.rangeClosed(1,threadCount)
+                .mapToObj(dateIntFunction)
+                .collect(Collectors.toList());
+        Collections.sort(resultList);
+        assertEquals(expectedList, resultList);
+    }
+
+    private Function<Future<Date>, Date> getFuture() {
+        return dateFuture -> {
+            try {
+                return dateFuture.get();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+            return null;
+        };
+
     }
 }
